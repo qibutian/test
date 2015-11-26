47a6f16e50251af144e47ea5c2cfa6bc5136d32d
qibutian <603986018@qq.com>
2015-11-26 22:56:38 +0800

    提交代码

--- foods/src/com/means/foods/hot/HotIndexFragment.java
+++ foods/src/com/means/foods/hot/HotIndexFragment.java
@@ -5,13 +5,20 @@ import in.srain.cube.views.ptr.PtrDefaultHandler;
 import in.srain.cube.views.ptr.PtrFrameLayout;
 import in.srain.cube.views.ptr.PtrHandler;
 import in.srain.cube.views.ptr.header.StoreHouseHeader;
+import in.srain.cube.views.ptr.loadmore.LoadMoreContainer;
+import in.srain.cube.views.ptr.loadmore.LoadMoreHandler;
+import in.srain.cube.views.ptr.loadmore.LoadMoreListViewContainer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.means.foods.R;
+import com.means.foods.adapter.TestAdapter;
 import com.means.foods.base.FoodsListFragment;
 import com.means.foods.collect.CollectIndexFragment;
 
@@ -22,6 +29,10 @@ public class HotIndexFragment extends FoodsListFragment {
 	View mainV;
 
 	PtrFrameLayout mPtrFrame;
+
+	TestAdapter adapter;
+
+	ListView listV;
 
 	public static HotIndexFragment getInstance() {
 		if (instance == null) {
@@ -43,20 +54,37 @@ public class HotIndexFragment extends FoodsListFragment {
 	}
 
 	private void initView() {
+		listV = (ListView) mainV.findViewById(R.id.listview);
 		mPtrFrame = (PtrFrameLayout) mainV.findViewById(R.id.ptr_frame);
-		final StoreHouseHeader header = new StoreHouseHeader(getActivity());
-		header.setPadding(0, DhUtil.dip2px(getActivity(), 15), 0, 0);
-		header.initWithString("Foods");
-		header.setTextColor(getResources().getColor(R.color.text_yellow));
-		mPtrFrame.addPtrUIHandler(header);
-		mPtrFrame.setHeaderView(header);
-		mPtrFrame.setPinContent(false);
+		final LoadMoreListViewContainer loadMoreListViewContainer = (LoadMoreListViewContainer) mainV
+				.findViewById(R.id.load_more_list_view_container);
+
+		initRefreshListView(mPtrFrame, loadMoreListViewContainer);
+
+//		loadMoreListViewContainer.setLoadMoreHandler(new LoadMoreHandler() {
+//			@Override
+//			public void onLoadMore(LoadMoreContainer loadMoreContainer) {
+//				mPtrFrame.postDelayed(new Runnable() {
+//
+//					@Override
+//					public void run() {
+//						adapter.setData(10);
+////						mPtrFrame.refreshComplete();
+//
+//						// load more
+//						loadMoreListViewContainer.loadMoreFinish(true, true);
+//					}
+//				}, 2000);
+//			}
+//		});
+
 		mPtrFrame.setPtrHandler(new PtrHandler() {
 			@Override
 			public void onRefreshBegin(PtrFrameLayout frame) {
 				frame.postDelayed(new Runnable() {
 					@Override
 					public void run() {
+						adapter.refresh();
 						mPtrFrame.refreshComplete();
 					}
 				}, 1800);
@@ -69,5 +97,16 @@ public class HotIndexFragment extends FoodsListFragment {
 						content, header);
 			}
 		});
+		mPtrFrame.postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				mPtrFrame.autoRefresh(true);
+			}
+		}, 150);
+
+		adapter = new TestAdapter(getActivity());
+		listV.setAdapter(adapter);
 	}
+
 }
