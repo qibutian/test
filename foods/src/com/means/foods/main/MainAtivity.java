efb304205056bcd36ed557580be845b291954c61
qibutian <603986018@qq.com>
2015-11-26 10:48:28 +0800

    提交代码

---
+++ foods/src/com/means/foods/main/MainAtivity.java
@@ -0,0 +1,17 @@
+package com.means.foods.main;
+
+import com.means.foods.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainAtivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		setContentView(R.layout.activity_main);
+	}
+
+}
