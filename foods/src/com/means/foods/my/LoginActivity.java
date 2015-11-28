201380a22930f5ef27edb2506d35d76cbb210ae0
qibutian <603986018@qq.com>
2015-11-28 17:19:59 +0800

    提交代码

---
+++ foods/src/com/means/foods/my/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.means.foods.my;
+
+import android.os.Bundle;
+
+import com.means.foods.base.FoodsBaseActivity;
+
+public class LoginActivity extends FoodsBaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
