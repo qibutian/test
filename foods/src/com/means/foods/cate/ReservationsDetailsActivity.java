c076d8a754273ae33de7ceca21c09b014ac85770
胡庆翰 <845643018@.qq.com>
2015-11-28 20:48:32 +0800

    更新代码

---
+++ foods/src/com/means/foods/cate/ReservationsDetailsActivity.java
@@ -0,0 +1,23 @@
+package com.means.foods.cate;
+
+import com.means.foods.R;
+import com.means.foods.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * 预定详情
+ * @author dell
+ *
+ */
+public class ReservationsDetailsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_reservations_details);
+	}
+}
