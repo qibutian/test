7c2ac043f9664fdb3220e70dd142dcb69a30992a
hu845643018 <845643018@qq.com>
2015-12-10 14:48:18 +0800

    更新代码
    更新代码

---
+++ foods/src/com/means/foods/ConfirmPaymentActivity.java
@@ -0,0 +1,28 @@
+package com.means.foods;
+
+import com.means.foods.base.FoodsBaseActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * 确认支付
+ * @author Administrator
+ *
+ */
+public class ConfirmPaymentActivity extends FoodsBaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_confirm_payment);
+	}
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+}
