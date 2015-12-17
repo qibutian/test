b2d12b7a2e7ef857e2b936f6cc16e543802ef48f
hu845643018 <845643018@qq.com>
2015-12-17 16:17:51 +0800

    更新代码

--- foods/src/com/means/foods/main/GuidanceActivity.java
+++ foods/src/com/means/foods/main/GuidanceActivity.java
@@ -56,5 +56,16 @@ public class GuidanceActivity extends FoodsBaseActivity {
 				finishWithoutAnim();
 			}
 		});
+		
+		findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(self, MainActivity.class);
+				startActivity(intent);
+				finishWithoutAnim();
+			}
+		});
 	}
 }
